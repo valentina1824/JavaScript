5050f021ff2317c03d283fe553a6a17dd8b7fca3
valentina1824 <linaresvalentina2418@gmail.com>
2021-05-04 17:51:36 -0500

    Ejercicio de JavaScript

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function imprimir(){
+
+    let impresion ="" 
+    impresion = document.getElementById("name").value + "\n"
+    impresion += document.getElementById("lastName").value + "\n"
+    impresion += document.getElementById("age").value + "\n"
+    impresion += document.getElementById("course").value + "\n"
+
+    alert(impresion)
+    console.log("Hola " + "a " + "todos")
+}
+console.log("Funcionando")
